ab7ddb9f4dc8f8717a6bae17503ff8c7a9da3f99
hailsalex91 <hails.alex91@gmail.com>
2014-03-31 14:03:04 -0400

    updated settings to link static files

--- playgroundFinder/settings.py
+++ playgroundFinder/settings.py
@@ -62,20 +62,23 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_PATH, 'static_media')
+
+###STATIC_ROOT = os.path.join(PROJECT_PATH, 'static_media')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+
+###STATIC_URL = '/static/'
 
 # Additional locations of static files
+'''
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(PROJECT_ROOT, 'static_media'),
 )
-
+'''
 # List of finder classes that know how to find static files in
 # various locations.
 
@@ -163,136 +166,13 @@ LOGGING = {
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(default=os.environ["HEROKU_POSTGRESQL_YELLOW_URL"])
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+### Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Allow all host headers
+### Allow all host headers
 ALLOWED_HOSTS = ['*']
 
-##############
-"""
-Django settings for sports project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
-PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
-PROJECT_PATH = os.path.dirname(PROJECT_ROOT)
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2qhx%sv27q&amp;zl=$vh1(ca^1td6_!j^l_7(q1kmjhy%iv7)e6ms'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
-
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.admindocs',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    #'django.contrib.sites',
-    'django.contrib.humanize',
-    'playgroundApp',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-ROOT_URLCONF = 'playgroundFinder.urls'
-
-# Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'playgroundFinder.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_PATH, 'playgroundFinder.db'),
-        'USER': '',         #not used in sqllite3
-        'PASSWORD': '',     #not used in sqllite3
-        'HOST': '',         #Default as an empty string, not used in sqllite3
-        'PORT': '',         #Default as an empty string, not used in sqllite3
-    }
-}
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media_root')
-MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(PROJECT_PATH, 'static_media')
-
-STATICFILES_DIR = (
-    os.path.join(PROJECT_ROOT, 'static_media'),
-)
-STATIC_URL = '/static/'
-TEMPLATE_DIRS = (
-    os.path.join(PROJECT_ROOT, 'templates'),
-)
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
-'''
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
-'''
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-#DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
-
-# Static asset configuration
-import os
+### Static asset configuration
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
@@ -300,4 +180,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-"""

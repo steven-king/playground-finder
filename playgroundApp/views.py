746d23b47ac2f0851a99e09f0e507bcb72578caa
hailsalex91 <hails.alex91@gmail.com>
2014-03-30 20:44:32 -0400

    made changes to models

--- playgroundApp/views.py
+++ playgroundApp/views.py
@@ -3,3 +3,30 @@ from django.http import HttpResponse
 
 def home(request):
         return HttpResponse('HelloWorld')
+
+# Below is copied from Xing with comments taken out
+# Create your views here.
+
+from django.shortcuts import render, render_to_response
+
+
+def playgroundDetail (request):
+	return render (request, "playgroundApp/playground_info.html")
+
+def playgroundList(request):
+	return render (request, "playgroundApp/home.html")
+
+def suggestPlayground(request):
+	return render (request, "playgroundApp/playground_suggest.html")
+
+def userProfile (request):
+	return render (request, "playgroundApp/user_profile.html")
+
+def userLogin (request):
+	return render (request, "playgroundApp/user_login.html")
+
+def userSignUp(request):
+	return  render (request, "playgroundApp/user_signup.html")
+
+def userSuggest(request):
+	return  render (request, "playgroundApp/user_suggest.html")

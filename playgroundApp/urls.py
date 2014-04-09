718e4d394c8466399f2984b29e5a8278a5903142
JOMC <jomc@mid-campus-03422.wireless.unc.edu>
2014-04-09 15:55:56 -0400

    forms and review

--- playgroundApp/urls.py
+++ playgroundApp/urls.py
@@ -10,8 +10,7 @@ urlpatterns=patterns('',
 	url(r'^playgroundapp/user_suggest$', views.userSuggest, name='userSuggest'),
 	url(r'^playgroundapp/user_signup$', views.userSignUp, name='userSignUp'),
 	url(r'^playgroundapp/user_login$', views.userLogin, name='userLogin'),
-        url(r'^playgroundapp/newreview/$', views.userReview, name='playground_new_review'),
-        url(r'^playgroundapp/new_review.html', views.userReview, name='post_form_review'),
+        url(r'^playgroundapp/newreview$', views.userReview, name='userReview'),
         url(r'^playgroundapp/map$', views.map, name='map'),
 )
 
